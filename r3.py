66d662e0cb9f7fa1933cba70235e8ab83a1878ba
Prashant Sakhuja <prashant.sakhuja@gmail.com>
2016-11-23 23:01:53 -0800

    r3 seems to be udpated

--- r3.py
+++ r3.py
@@ -1,1 +1,35 @@
 #this is the main class that auto runs when r3 raspian OS starts.
+
+
+
+
+
+def key_input(event):
+    init()
+    print "Key:", event.char
+    key_press = event.char
+    sleep_time = 0.060
+
+    if key_press.lower() == "w":
+        forward(sleep_time)
+    elif key_press.lower() == "s":
+        reverse(sleep_time)
+    elif key_press.lower() == "a":
+        turn_left(sleep_time)
+    elif key_press.lower() == "d":
+        turn_right(sleep_time)
+    elif key_press.lower() == "p":
+        stop(sleep_time)
+    else:
+        pass
+
+    curDis = distance("cm")
+    print("Distance:", curDis)
+
+    if curDis <15:
+        init()
+        reverse(0.5)
+
+command = tk.TK()
+command.bind('<keypress>', key_input)
+command.mainloop()
