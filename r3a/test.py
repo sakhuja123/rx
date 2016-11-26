bc8bda007f4d26d596765666fe792cee2a791367
Prashant Sakhuja <prashant.sakhuja@gmail.com>
2016-11-26 00:14:31 -0800

    quick push

--- r3a/test.py
+++ r3a/test.py
@@ -5,13 +5,15 @@ from r3s import get_dist
 print "test start"
 r = r3c.r3c() #initialize chassis
 
-print "fwd"
-r.nav(65,65,5)
-print "backup"
-r.nav(-65,-65,5)
 print "pivot left"
 r.pivot('left')
 print "pivot right"
 r.pivot('right')
+
+print "fwd"
+r.nav(65,65,5)
+print "backup"
+r.nav(-65,-65,5)
+
 
 print "test over!"
