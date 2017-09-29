08fe22c0d37e5616f7463352d8a3a4a2d60b9357
Prashant Sakhuja <prashant.sakhuja@gmail.com>
2017-09-29 00:14:18 -0700

    quick commit

--- r3a/tc.py
+++ r3a/tc.py
@@ -7,16 +7,27 @@ print "test start, initializing chasis and sensors"
 r = r3c.r3c() #initialize chassis
 
 
-print "@@@@@@@@@@ test journey start"
-r.move(50,50)
-r.journey('random',90)
-print "@@@@@@@@@@ test journey end"
+print "@@@@@@@@@@ test random journey start"
+print "@@@@@@@@@@ test random journey start"
+print "@@@@@@@@@@ test random journey start"
+print "@@@@@@@@@@ test random journey start"
+print "@@@@@@@@@@ test random journey start"
+print "@@@@@@@@@@ test random journey start"
+#r.move(50,50)
+#r.journey('random',90)
+print "@@@@@@@@@@ test random journey end"
+print "@@@@@@@@@@ test random journey end"
+print "@@@@@@@@@@ test random journey end"
+print "@@@@@@@@@@ test random journey end"
+print "@@@@@@@@@@ test random journey end"
 
 
-print "nav test 60 secs"
-r.nav(-100,-100,-1)
-r.nav(100,100,60)
+print "nav front 3secs back 3secs"
+r.nav(-100,-100,-3)
+r.nav(100,100,3)
 r.stop()
+print "STOPPING nav front 3secs back 3secs"
+
 
 print "pivot left"
 r.pivot('left')
